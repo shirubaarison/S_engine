4d699cc75059f10ea4dbfce1db4c595d628ba386
Alysson Silva <shirubaarison@gmail.com>
2026-03-01 16:17:43 -0300

    add GameObject

---
+++ src/core/GameObject.cpp
@@ -0,0 +1,40 @@
+#include "core/GameObject.h"
+
+namespace {
+  static glm::mat4 getTransformationMatrix(const GameObject& obj) {
+
+    glm::mat4 model = glm::mat4{1.0f};
+    model = glm::translate(model, obj.getPosition());
+
+    glm::vec3 rotation = obj.getRotation();
+    model = glm::rotate(model, rotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
+    model = glm::rotate(model, rotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
+    model = glm::rotate(model, rotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
+
+    model = glm::scale(model, obj.getScale());
+
+    return model;
+  }
+}
+
+GameObject::GameObject(std::shared_ptr<Model>, glm::vec3 pos, glm::vec3 rot, glm::vec3 scale)
+  : m_position(pos),
+    m_rotation(rot),
+    m_scale(scale) {}
+
+void GameObject::render(Shader& shader, Camera& camera)
+{
+  if (!m_model)
+    return;
+
+  glm::mat4 view = camera.getViewMatrix();
+  glm::mat4 projection = camera.getProjectionMatrix();
+
+  glm::mat4 modelMatrix = getTransformationMatrix(*this);
+
+  shader.setMatrix4("view", view);
+  shader.setMatrix4("projection", projection);
+  shader.setMatrix4("model", modelMatrix);
+
+  m_model->draw(shader);
+}
---
+++ src/core/GameObject.h
@@ -0,0 +1,36 @@
+#ifndef GAME_OBJECT_H
+#define GAME_OBJECT_H
+
+#include "graphics/Camera.h"
+#include "graphics/Model.h"
+#include "graphics/Shader.h"
+#include <glm/vec3.hpp>
+#include <memory>
+
+class GameObject {
+public:
+  GameObject() = default;
+  GameObject(std::shared_ptr<Model>,
+             glm::vec3 pos = {0, 0, 0},
+             glm::vec3 rot = {0, 0, 0},
+             glm::vec3 scale = { 1, 1, 1});
+
+  virtual ~GameObject() = default;
+  virtual void update(float deltaTime) { (void)deltaTime; } // supressing unused variable warning xD
+  void render(Shader& shader, Camera& camera);
+
+  const glm::vec3& getPosition() const { return m_position; }
+  const glm::vec3& getRotation() const { return m_rotation; }
+  const glm::vec3& getScale() const { return m_scale; }
+
+  void setPosition(const glm::vec3& pos) { m_position = pos; }
+  void setModel(std::shared_ptr<Model> model) { m_model = model; }
+
+protected:
+  glm::vec3 m_position{0, 0, 0};
+  glm::vec3 m_rotation{0, 0, 0};
+  glm::vec3 m_scale{1, 1, 1};
+  std::shared_ptr<Model> m_model;
+};
+
+#endif // !GAME_OBJECT_H
