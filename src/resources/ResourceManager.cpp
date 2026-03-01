19110b46733b732fc3bb826882a8f1753f2ad097
Alysson Silva <shirubaarison@gmail.com>
2026-03-01 18:54:13 -0300

    add support for models in ResourceManager

--- src/resources/ResourceManager.cpp
+++ src/resources/ResourceManager.cpp
@@ -1,13 +1,19 @@
 #include "resources/ResourceManager.h"
+#include <memory>
 
 std::map<std::string, Shader> ResourceManager::shaders;
+std::map<std::string, std::shared_ptr<Model>> ResourceManager::models;
 
-Shader &ResourceManager::loadShader(const std::string &name, const char *vertexPath, const char *fragmentPath) {
+Shader& ResourceManager::loadShader(const std::string& name, const char* vertexPath, const char* fragmentPath)
+{
   shaders[name] = Shader(vertexPath, fragmentPath);
 
   return shaders[name];
 }
 
-Shader &ResourceManager::getShader(const std::string &name) {
-  return shaders[name];
+std::shared_ptr<Model> ResourceManager::loadModel(const std::string& name, const std::string& path)
+{
+  models[name] = std::make_shared<Model>(path);
+
+  return models[name];
 }
--- src/resources/ResourceManager.h
+++ src/resources/ResourceManager.h
@@ -1,19 +1,24 @@
 #ifndef RESOURCE_MANAGER_H
 #define RESOURCE_MANAGER_H
 
+#include "graphics/Model.h"
 #include "graphics/Shader.h"
 #include <map>
+#include <memory>
 
 class ResourceManager {
 public:
   ResourceManager() = delete;
 
-  static Shader &loadShader(const std::string &name, const char *vertexPath, const char *fragmentPath);
-  static Shader &getShader(const std::string &name);
+  static Shader &loadShader(const std::string& name, const char* vertexPath, const char* fragmentPath);
+  static Shader &getShader(const std::string& name) { return shaders[name]; }
 
-  void clear();
+  static std::shared_ptr<Model> loadModel(const std::string& name, const std::string& path);
+  static std::shared_ptr<Model> getModel(const std::string& name) { return models[name]; }
+
 private:
   static std::map<std::string, Shader> shaders;
+  static std::map<std::string, std::shared_ptr<Model>> models;
 };
 
 #endif // !RESOURCE_MANAGER_H
