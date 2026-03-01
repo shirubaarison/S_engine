2e2224eecc7edfaed456aa443f77e66105b4415b
Alysson Silva <shirubaarison@gmail.com>
2026-02-28 23:40:56 -0300

    enable depth, clear buffers add drawModelMethod and aspectRatio

--- src/core/Renderer.cpp
+++ src/core/Renderer.cpp
@@ -6,10 +6,15 @@
 
 #include "utils/errorReporting.h"
 
-Renderer::Renderer(int width, int height) : m_width(width), m_height(height) {}
+Renderer::Renderer(int width, int height) : m_width(width), m_height(height)
+{
+  m_aspectRatio = static_cast<float>(m_width) / static_cast<float>(m_height);
+}
+
 Renderer::~Renderer() {}
 
-bool Renderer::init() {
+bool Renderer::init()
+{
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     std::cerr << "GLAD initialization failed.\n";
     return false;
@@ -17,16 +22,35 @@ bool Renderer::init() {
   enableReportGlErrors();
 
   glEnable(GL_BLEND);
+  glEnable(GL_DEPTH_TEST);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   glViewport(0, 0, m_width, m_height);
 
   return true;
 }
 
-void Renderer::draw() {
+void Renderer::draw()
+{
 
 }
 
-void Renderer::render() {
+void Renderer::drawModel(const Model& model, Shader& shader, const Camera& camera, const glm::mat4& modelMatrix)
+{
+  shader.use();
 
+  shader.setMatrix4("model", modelMatrix);
+  shader.setMatrix4("view", camera.getViewMatrix());
+  shader.setMatrix4("projection", camera.getProjectionMatrix(m_aspectRatio));
+
+  shader.setVector3f("lightPos", glm::vec3(2.0f, 4.0f, 3.0f));
+  shader.setVector4f("lightColor", glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
+  shader.setVector3f("camPos", camera.getPosition());
+
+  model.draw(shader);
+}
+
+void Renderer::render()
+{
+  glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
--- src/core/Renderer.h
+++ src/core/Renderer.h
@@ -1,6 +1,10 @@
 #ifndef RENDERER_CLASS_H
 #define RENDERER_CLASS_H
 
+#include "graphics/Camera.h"
+#include "graphics/Model.h"
+#include "graphics/Shader.h"
+
 class Renderer {
 public:
   Renderer(int width, int height);
@@ -9,11 +13,14 @@ public:
   bool init();
 
   void draw();
+  void drawModel(const Model& model, Shader& shader, const Camera& camera, const glm::mat4& modelMatrix);
   void render();
 
 private:
   int m_width;
   int m_height;
+
+  int m_aspectRatio;
 };
 
 #endif // !RENDERER_CLASS_H
